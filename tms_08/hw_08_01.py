01ad502f45dd6b6f62a4e89596fd30161ec94154
saymeoow <meowizy94@gmail.com>
2022-10-10 22:19:22 +0300

    end ex1

--- tms_08/hw_08_01.py
+++ tms_08/hw_08_01.py
@@ -1,14 +1,23 @@
+import datetime
+
 class Employee:
     def __init__(self, first_name=str, last_name=str, age=int, profession=str):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.profession = profession
-        self.onboarding_time = 0
+        self.onboarding_time = datetime.date(2007,4,23)
 
-    def onboarding_time(self):
-        if any(x < 0 for x in self.onboarding_time):
-            raise ValueError('value muse be positive')
+    def info(self):
+        return print(dict({'fullname': self.first_name+self.last_name,
+                           'age': self.age,
+                           'working_time': (str(2022-self.onboarding_time.year)+' years',
+                                            str(12-self.onboarding_time.month)+' months',
+                                            str(30-self.onboarding_time.day)+' days')}))
+
+petr=Employee(first_name='Petr',last_name='Jess',age=22,profession='doc' )
+petr.info()
+
 
 
 
