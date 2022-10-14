a26cc75fd97b6bd8ac5b0332541fd442cb4bf0ac
saymeoow <meowizy94@gmail.com>
2022-10-14 18:53:12 +0300

    ex 1 complete

--- tms_08/hw_08_01.py
+++ tms_08/hw_08_01.py
@@ -1,22 +1,27 @@
-import datetime
-
 class Employee:
     def __init__(self, first_name=str, last_name=str, age=int, profession=str):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.profession = profession
-        self.onboarding_time = datetime.date(2007,4,23)
+        self.onboarding_time = None
+
+    @property
+    def onb_time(self):
+        if self.onboarding_time is None:
+            self.onboarding_time = f"21.4.2019"
+        return self.onboarding_time
+
 
     def info(self):
         return print(dict({'fullname': self.first_name+self.last_name,
                            'age': self.age,
-                           'working_time': (str(2022-self.onboarding_time.year)+' years',
-                                            str(12-self.onboarding_time.month)+' months',
-                                            str(30-self.onboarding_time.day)+' days')}))
+                           'working_time': petr.onb_time}))
 
 petr=Employee(first_name='Petr',last_name='Jess',age=22,profession='doc' )
 petr.info()
+
+
 
 
 
