01dd10894243bf3ef82f353849e5a3aa1cb90748
saymeoow <meowizy94@gmail.com>
2022-10-16 13:40:32 +0300

    hw_07_04 complete

--- tms_07/hw_07_04.py
+++ tms_07/hw_07_04.py
@@ -6,22 +6,24 @@ from base import Shape
 class Box:
 
     def __init__(self):
-        self.shapes = 0
+        self.shapes = []
 
-    def add_shape(self, inst):
-        if isinstance(inst, Shape):
-            self.shapes += inst.get_area()
+    def add_shape(self, sh):
+        if isinstance(sh, Shape):
+            return self.shapes.append(sh)
 
     def remove_shape(self):
         self.shapes.pop()
 
     def get_common_area(self):
-        return sum((x.get_area for x in self.shapes))
+        return int(sum(sh.get_area() for sh in self.shapes))
 
-def main():
-    box = Box()
-    box.add_shape(Circle(3))
-    box.add_shape(Rectangle(2, 4))
-    assert box.get_common_area() == 36
 
-main()
+box = Box()
+box.add_shape(Rectangle(2, 4))
+box.add_shape(Circle(3))
+assert box.get_common_area() == 36
+box.add_shape(Circle(4))
+assert box.get_common_area() == 86
+box.remove_shape()
+assert box.get_common_area() == 36
